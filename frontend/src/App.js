d42960a2a230d4ecd98f8948c4acfe3a0ef9573f
Jacquelie-jv <jacqueline.jimenez@est.iudigital.edu.co>
2025-04-04 09:52:02 -0500

    Actualización de archivos modificados

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar"; // Asegura que la ruta es correcta
-import Home from "./pages/Home"; // Asegura que Home está en /pages
-import MovieDetail from "./pages/MovieDetail"; // Asegura que MovieDetail está en /pages
-import "tailwindcss/tailwind.css";
-import "./App.css";
+import Navbar from "./components/Navbar"; // Corrige el nombre si es necesario
+import Home from "./pages/Home";
+import MovieDetail from "./pages/MovieDetail";
+import Peliculas from "./components/Peliculas";
+import "./index.css"; // Asegúrate de que Tailwind está en este archivo
 
 const App = () => {
   return (
     <Router>
-      <div className="bg-gray-900 min-h-screen text-white"> {/* Fondo oscuro para todo */}
-        <Navbar /> {/* Navbar siempre visible */}
-        <div className="container mx-auto p-4"> {/* Contenedor para centrar contenido */}
+      <div className="bg-gray-900 min-h-screen text-white">
+        <Navbar />
+        <div className="container mx-auto p-4">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/movie/:id" element={<MovieDetail />} />
+            <Route path="/peliculas" element={<Peliculas />} />
           </Routes>
         </div>
       </div>
@@ -23,6 +24,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
