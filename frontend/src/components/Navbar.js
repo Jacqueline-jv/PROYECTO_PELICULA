d119e4060f6c737708127a49216565b034199bdc
Jacquelie-jv <jacqueline.jimenez@est.iudigital.edu.co>
2025-04-04 09:53:32 -0500

    Corrección: Se agregan archivos modificados

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Nabar = () => {
+const Navbar = () => {
   return (
     <nav className="bg-black p-4 text-white flex justify-between">
       <h1 className="text-xl">🎬 Mi Aplicación de Películas</h1>
@@ -15,4 +15,4 @@ const Nabar = () => {
   );
 };
 
-export default Nabar;
+export default Navbar;
